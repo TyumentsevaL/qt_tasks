a69c9248ab6002dac667f08497cd8dd3751dd3b1
Lubov Tyumentseva <tl442@mail.ru>
2021-03-11 01:58:43 +0300

    Task1: draft polished
    - написаны комментарии
    - FileInfoWatcher упрощён

--- task_1/fileinfowatcher.cpp
+++ task_1/fileinfowatcher.cpp
@@ -2,7 +2,6 @@
 
 #include <QFileInfo>
 #include <QDateTime>
-#include <QDebug>
 
 FileInfoWatcher::FileInfoWatcher(const QString &watchedFilePath, QObject *parent)
     : QObject(parent)
@@ -16,31 +15,25 @@ void FileInfoWatcher::setWatchedFile(const QString &path)
 
 QString FileInfoWatcher::getInfoToString() const
 {
-    if (m_fileData.empty()) {
+    if (m_watchedFileInfo.fileName().isEmpty()) {
         return QString("No data yet for path: %1").arg(m_watchedFilePath);
     }
 
-    if (!m_fileData[EXISTS_INFO].toBool()) {
+    if (!m_watchedFileInfo.exists()) {
         return QString("File removed or never existed: %1").arg(m_watchedFilePath);
     }
 
     return QString("Watched path -- %1\n  Name: %2\n  Size: %3\n  Last modified: %4\n\n")
             .arg(m_watchedFilePath)
-            .arg(m_fileData[BASE_NAME_INFO].toString())
-            .arg(m_fileData[SIZE_INFO].toLongLong())
-            .arg(m_fileData[LAST_MODIFIED_INFO].toDateTime().toString(Qt::ISODate));
+            .arg(m_watchedFileInfo.baseName())
+            .arg(m_watchedFileInfo.size())
+            .arg(m_watchedFileInfo.lastModified().toString(Qt::ISODate));
 }
 
 void FileInfoWatcher::fileInfoUpdated(const QFileInfo &newInfo)
 {
     if (newInfo.filePath() == m_watchedFilePath) {
-        m_fileData[EXISTS_INFO] = newInfo.exists();
-        if (newInfo.exists()) {
-            m_fileData[SIZE_INFO] = newInfo.size();
-            m_fileData[BASE_NAME_INFO] = newInfo.baseName();
-            m_fileData[LAST_MODIFIED_INFO] = newInfo.lastModified();
-        }
-
+        m_watchedFileInfo = newInfo;
         emit fileChanged();
     }
 }
--- task_1/filewatchermanager.h
+++ task_1/filewatchermanager.h
@@ -10,6 +10,7 @@ QT_FORWARD_DECLARE_CLASS(QTimer)
  * \brief The FileWatcherManager class это менеджер,
  * который наблюдает за файлами. Названия списаны с QFileSystemWatcher, но
  * принцип работы проще -- по таймеру проверяет список файлов, есть ли изменения.
+ * Это или удаление, или дата изменения поменялась.
  * Сделан как синглтон, согласно заданию. НЕ потокобезопасен.
  */
 class FileWatcherManager : public QObject
@@ -23,17 +24,45 @@ public:
         return instance;
     }
 
+    /*!
+     * \brief addPath -- добавляет файл для наблюдения
+     * \param path    -- путь до файла
+     * \return        -- если это папка или файла нет, то false
+     */
     bool addPath(const QString &path);
 
+    /*!
+     * \brief addPaths -- добавляет пачкой файлы
+     * \param paths    -- пути до наблюдаемых файлов
+     * \return         -- список файлов, которые добавить не вышло
+     */
     QStringList addPaths(const QStringList &paths);
 
+    /*!
+     * \brief files -- список наблюдаемых файлов возвращает
+     * \return
+     */
     QStringList files() const;
 
+    /*!
+     * \brief removePath -- удаляет путь из наблюдения
+     * \param path       -- что удалить
+     * \return           -- если такого файла уже нет, ничего не удалится
+     */
     bool removePath(const QString &path);
 
+    /*!
+     * \brief removePaths -- удаляет несклько путей из наблюдения
+     * \param paths       -- что удалить
+     * \return            -- список файлов, которые удалить из наблюдения не вышло
+     */
     QStringList removePaths(const QStringList &paths);
 
 signals:
+    /*!
+     * \brief fileChanged -- сигнал, что что-то поменялось в одном из наблюдаемых файлов
+     * \param newInfo     -- новые данные о файле
+     */
     void fileChanged(const QFileInfo& newInfo);
 
 private:
